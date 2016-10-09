41c19f0c1b593955a6f178469aa668421bba4593
Marek Slaninka <slaninka@mofa.sk>
2016-10-09 23:10:11 +0200

    SwiftGPIO

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "myHomeServer_vapor",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 0, minor: 8)
     ]
 )
--- Sources/myHomeServer_vapor/Controllers/GateController.swift
+++ Sources/myHomeServer_vapor/Controllers/GateController.swift
@@ -7,9 +7,19 @@
 //
 
 import Foundation
+import SwiftyGPIO
 
 final class GateController {
     static let sharedInstance = GateController()
+    
+    init() {
+        
+        let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPi2)
+
+        guard let ledGPIO = gpios[.P4] else {
+            fatalError("It has not been possible to initialised the LED GPIO pin")
+        }
+    }
     
     func openGate() {
         
