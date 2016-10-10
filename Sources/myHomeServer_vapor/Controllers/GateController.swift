69a75764f7e786d329b3a009ff8fea643de8627c
Marek Slaninka <slaninka@mofa.sk>
2016-10-10 20:26:51 +0200

    test blinkl

--- Sources/myHomeServer_vapor/Controllers/GateController.swift
+++ Sources/myHomeServer_vapor/Controllers/GateController.swift
@@ -11,17 +11,28 @@ import SwiftyGPIO
 
 final class GateController {
     static let sharedInstance = GateController()
+    let gpioForGate: GPIO?
+    let highStateDuration: TimeInterval
+    let lowStateDuration: TimeInterval
     
     init() {
         
         let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPi2)
-
-        guard let ledGPIO = gpios[.P4] else {
+        self.gpioForGate = gpios[.P4]
+        guard self.gpioForGate != nil else {
             fatalError("It has not been possible to initialised the LED GPIO pin")
         }
+        self.highStateDuration = 3
+        self.lowStateDuration = 0.5
     }
     
     func openGate() {
-        
+        guard self.gpioForGate != nil else {
+            fatalError("It has not been possible to initialised the LED GPIO pin")
+        }
+
+        self.gpioForGate?.value = 1
+        sleep(UInt32(round(highStateDuration)))
+        self.gpioForGate?.value = 0
     }
 }
--- Sources/myHomeServer_vapor/main.swift
+++ Sources/myHomeServer_vapor/main.swift
@@ -10,7 +10,12 @@ drop.get("/tomas") { _ in
     return "Hello Tomas"
 }
 
-
+drop.get("/start") { _ in
+    
+    GateController.sharedInstance.openGate()
+    return "otvoreke"
+    
+}
 
 
 drop.run()
