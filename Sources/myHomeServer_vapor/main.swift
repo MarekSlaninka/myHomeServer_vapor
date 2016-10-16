588bd27f1e32993290c22270f3157f1354f8ae47
Marek Slaninka <slaninka@mofa.sk>
2016-10-16 13:35:52 +0200

    no message

--- Sources/myHomeServer_vapor/main.swift
+++ Sources/myHomeServer_vapor/main.swift
@@ -24,10 +24,25 @@ drop.get("/temp") { _ in
     DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
         drop.console.print("background after", newLine: true)
     }
+    
+    
 
     return TempController.sharedInstance.getTemp()
 }
 
 
+drop.get("/background") { _ in
+    
+    DispatchQueue.global().async {
+        var i = 0
+        while i < 50 {
+            i+=1
+            drop.console.print("background after", newLine: true)
+            sleep(2)
+        }
+    }
+    return "background"
+    
+}
 
 drop.run()
