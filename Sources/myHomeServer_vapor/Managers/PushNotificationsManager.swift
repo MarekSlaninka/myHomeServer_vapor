0fa3b3db50eb14e57c6dee33a959b56e067f4877
Marek Slaninka <slaninka@mofa.sk>
2016-11-12 11:13:21 +0100

    Notifikacie funkcne

--- Sources/myHomeServer_vapor/Managers/PushNotificationsManager.swift
+++ Sources/myHomeServer_vapor/Managers/PushNotificationsManager.swift
@@ -16,16 +16,6 @@ class PushNotificationsManager: NSObject {
     
     
     func sendNotification(withTitle title: String, body: String, completitionBlock: ((String?)->())?, drop: Droplet) -> Response? {
-        
-        
-        do {
-            let spotifyResponse = try drop.client.get("https://api.weixin.qq.com/cgi-bin/token")
-            print(spotifyResponse)
-        } catch let error  {
-            print(error)
-        }
-        
-        
         let param = [
             "to": "fVaWMgWu35M:APA91bFBOVsrJ8R-D3MZ8vvWRibupTEHA-InkaJqOhaMhxaTkKIPHfpzswxnguVQb4Zlci1SptFX8qKZYTurh45w-Mo_yk1A6gv8UwwllwhmucQjPibNwElQW9DCm_TCChMmvgk8j4lI",
             "priority": 10,
@@ -34,53 +24,23 @@ class PushNotificationsManager: NSObject {
                 "body": body,
                 "badge": 0,
                 "sound":"default"
-            ],
+            ]
 //            "data": [
 //                "attachment-url": "https://api.buienradar.nl/Image/1.0/RadarMapNL"
 //            ]
         ] as [String : Any]
-        
-//        var request = URLRequest(url: URL(string: Config().pushFirebaseUrl)!)
-//        request.httpMethod = "POST"
-//        request.httpBody = try? JSONSerialization.data(withJSONObject: param, options: .prettyPrinted)
-//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.addValue("Authorization", forHTTPHeaderField: "key="+Config().pushAuthKey)
-        let jsonBody = try? JSONSerialization.data(withJSONObject: param, options: .prettyPrinted)
-        let heders = ["application/json":"Content-Type","Authorization":"key="+Config().pushAuthKey]
 
-        
+        let jsonBody = try? JSONSerialization.data(withJSONObject: param, options: .prettyPrinted)
+        let jsonString = String.init(data: jsonBody!, encoding: String.Encoding.ascii)
          do {
-           let response = try drop.client.post(Config().pushFirebaseUrl, headers: ["application/json":"Content-Type","Authorization":"key="+Config().pushAuthKey], query: param as! [String : CustomStringConvertible], body: Body.data([]))
+           let response = try drop.client.post(Config().pushFirebaseUrl, headers: ["Content-Type":"application/json","Authorization":"key="+Config().pushAuthKey], query: [:], body: jsonString!)
+            
+            debugPrint(response)
+            
             return response
         } catch  {
             debugPrint(error)
         }
-        let response = try? drop.client.post(Config().pushFirebaseUrl, headers: ["application/json":"Content-Type","Authorization":"key="+Config().pushAuthKey], query: param as! [String : CustomStringConvertible], body: Body.data((jsonBody?.makeBytes())!))
-        
-        return response
-        
-//        URLSession.shared.dataTask(with: request,
-//            completionHandler: { (data, response, error) in
-//                guard error == nil else {return}
-//                guard data != nil else {return}
-//                var dict: [String: AnyObject]?
-//                dict = try? JSONSerialization.jsonObject(with: data!,
-//                                                         options: .allowFragments) as! [String: AnyObject]
-//                if let dict = dict {
-//                    if let success = dict["success"] as? Int {
-//                        if let failure = dict["failure"] as? Int {
-//                            if success == 1 && failure == 0 {
-//                                //Push notifikacia presla
-//                                
-//                            }
-//                            completitionBlock?("\(success)")
-//                        }
-//                    }
-//                }
-//                //Spustenie vykonávania dátového tasku
-//        }).resume()
+        return nil
     }
-    
-    
-    
 }
