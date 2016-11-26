0ae804c75126dfc939d1b83b8085ecfa704c0416
Marek Slaninka <slaninka@mofa.sk>
2016-11-26 15:06:19 +0100

    no message

--- Sources/myHomeServer_vapor/Managers/ConfigManager.swift
+++ Sources/myHomeServer_vapor/Managers/ConfigManager.swift
@@ -25,45 +25,34 @@ class ConfigManager: NSObject {
     }
     
     func loadPropertyList() {
-        var plistData: [String: AnyObject] = [:] //Our data
-        //        if let configRoute: URL = URL(string: "file://" + drop.workDir.finished(with: "/") + "Config/thermoProbes.json") {
-        //            if let config = try? Data.init(contentsOf: configRoute) {
         
-        //        let plistXML = FileManager.default.contents(atPath: plistPath!)!
-        if let plistXML = try? Data.init(contentsOf: URL(string: self.plistPath + self.plistName)!) {
-            do {//convert the data to a dictionary and handle errors.
-                plistData = try PropertyListSerialization.propertyList(from: plistXML, options: [], format: &self.propertyListForamt) as! [String:AnyObject]
-                self.config = plistData
-                self.lastChange = plistData["lastChange"] as? Date
-            } catch {
-                print("Error reading plist: \(error), format: \(propertyListForamt)")
+        if let data = try? Data.init(contentsOf: URL(string: self.plistPath + self.plistName)!) {
+            if let config = try? Jay().anyJsonFromData([UInt8](data)) {
+                if let cf = config as? [String: Any] {
+                    drop.console.print(cf.description, newLine: true)
+                }
             }
+            
+
+            
         }
+//        
+//        var plistData: [String: AnyObject] = [:] //Our data
+//        if let plistXML = try? Data.init(contentsOf: URL(string: self.plistPath + self.plistName)!) {
+//            do {//convert the data to a dictionary and handle errors.
+//                plistData = try PropertyListSerialization.propertyList(from: plistXML, options: [], format: &self.propertyListForamt) as! [String:AnyObject]
+//                self.config = plistData
+//                self.lastChange = plistData["lastChange"] as? Date
+//            } catch {
+//                print("Error reading plist: \(error), format: \(propertyListForamt)")
+//            }
+//        }
     }
     
     func saveConfigToPlist() {
         drop.console.print(self.plistPath + self.plistName, newLine: true)
         let data = try? Jay(formatting: .prettified).dataFromJson(any: self.config) // [UInt8]
-
         try? Data.init(bytes: data!).write(to: URL(string:self.plistPath + self.plistName)!)
-
-        
-        
-//        do {
-//            data = try JSONSerialization.data(withJSONObject: self.config, options: JSONSerialization.WritingOptions.prettyPrinted)
-//            //        let data = try? PropertyListSerialization.data(fromPropertyList: self.config as? Any, format: self.propertyListForamt, options: 0)
-//            drop.console.print(self.plistPath + self.plistName, newLine: true)
-//        }catch {
-//            drop.console.print("serializationh error :" + (error as! String))
-//        }
-//        
-//        if data != nil {
-//            do {
-//                try data?.write(to: URL(string:self.plistPath + self.plistName)!)
-//            }catch {
-//                drop.console.print(error as! String)
-//            }
-//        }
     }
     
     func writeToConfig(object: Any, forKey key: String) {
