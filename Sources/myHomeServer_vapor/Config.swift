6d5ef2d1d3a2c7c4fe21c251209cec1e6481272d
Marek Slaninka <slaninka@mofa.sk>
2016-12-11 15:05:16 +0100

    Revert "Update Config.swift"
    This reverts commit 334e9866b0558d2a5606e5e0f207b0640599111e.

--- Sources/myHomeServer_vapor/Config.swift
+++ Sources/myHomeServer_vapor/Config.swift
@@ -9,7 +9,8 @@
 import Foundation
 
 class Config: NSObject {
- 
+    public let pushAuthKey = "AIzaSyDaFluLsUsb2g5mT2USjZnqjPIUH-SDT90"
+    public let pushFirebaseUrl = "https://fcm.googleapis.com/fcm/send"
 
     
 }
